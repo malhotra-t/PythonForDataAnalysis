6fbac0629f2cf8b26ad326b3a7d4097ab62ef94c
Tanya Malhotra <tanya.malhotra@outlook.com>
2017-04-05 16:06:51 -0400

    Asgt3 Q1 part1

--- Asgt3/Asgt3Q1Part1.py
+++ Asgt3/Asgt3Q1Part1.py
@@ -3,7 +3,7 @@
 
 # ## NYC Collision Analysis
 
-# In[ ]:
+# In[1]:
 
 import numpy as np
 import pandas as pd
@@ -11,42 +11,42 @@ import datetime
 import os
 
 
-# In[ ]:
+# In[2]:
 
 # Read csv file into a dataframe
 df = pd.read_csv('Data/vehicle_collisions.csv', index_col = 0, parse_dates=[1])
 
 
-# In[ ]:
+# In[3]:
 
 df.head()
 
 
-# In[ ]:
+# In[4]:
 
 # Filter to get all 2016 records
 df = df[ (df['DATE'] > datetime.date(year=2015,month=12,day=31)) & (df['DATE'] < datetime.date(year=2017,month=1,day=1))]
 
 
-# In[ ]:
+# In[5]:
 
 # Extract month from the date column
 df['MONTH']= df['DATE'].apply(lambda x: x.month)
 
 
-# In[ ]:
+# In[6]:
 
 # Filter records for Manhattan
 df_manhattan = df[df['BOROUGH'] == 'MANHATTAN']
 
 
-# In[ ]:
+# In[7]:
 
 # Group all the records for each month
 df = df.groupby(by='MONTH').count()
 
 
-# In[ ]:
+# In[8]:
 
 # Renaming the Date column header to indicate the count of collisions in NYC in the new dataframe
 df['NYC'] = df['DATE']
@@ -54,7 +54,7 @@ df = df['NYC']
 df.head()
 
 
-# In[ ]:
+# In[9]:
 
 # Group the records in Manhattan dataframe for each month
 df_manhattan = df_manhattan.groupby(by='MONTH').count()
@@ -63,31 +63,31 @@ df_manhattan['MANHATTAN'] = df_manhattan['DATE']
 df_manhattan = df_manhattan['MANHATTAN']
 
 
-# In[ ]:
+# In[10]:
 
 # Concatenating the dataframes for Manhattan and NYC
 final_percentage = pd.concat([df_manhattan,df], axis=1)
 
 
-# In[ ]:
+# In[11]:
 
 # Calculating the percentage of collisions in Manhattan to NYC and adding the column in the final dataframe
 final_percentage['PERCENTAGE'] = final_percentage['MANHATTAN'] / final_percentage['NYC']
 
 
-# In[ ]:
+# In[12]:
 
 # Creating the output folder
 os.makedirs('Output', exist_ok=True)
 
 
-# In[ ]:
+# In[13]:
 
 # Saving the final csv in the Output folder
 final_percentage.to_csv('Output/Q1Part1.csv')
 
 
-# In[ ]:
+# In[14]:
 
 print('Completed the analysis for calculating percentage of collisions in Manhattan compared to those in NYC for each month. Output is saved as a csv file in the location Output/Q1Part1.csv')
 
